ac86ddf3b39a2d5548c3fbba390388c15bd9ac54
Paul Zerr <paulzerr1@gmail.com>
2018-02-02 23:03:29 +0100

    Neue Funktionen und Tests

--- src/main/java/marsrover/Rover.java
+++ src/main/java/marsrover/Rover.java
@@ -5,6 +5,7 @@ public class Rover {
     private String endPunkt = "N:0:0";
 
     private String direction = "N";
+    private int x;
     private int y;
 
     String endPunkt() {
@@ -14,29 +15,68 @@ public class Rover {
     void move(char[] commands) {
         for (char command : commands) {
             if (command == 'f') {
-                this.y++;
+                moveForward();
+            } else if  (command == 'b') {
+                moveBackword();
             } else if (command == 'r') {
-                if ("N".equals(direction)) {
-                    direction = "E";
-                } else if ("E".equals(direction)) {
-                    direction = "S";
-                } else if ("S".equals(direction)) {
-                    direction = "W";
-                } else {
-                    direction = "N";
-                }
+                rotateRight();
+            } else if (command == 'l'){
+                rotateLeft();
             } else {
-                if ("N".equals(direction)) {
-                    direction = "W";
-                } else if ("E".equals(direction)) {
-                    direction = "N";
-                } else if ("S".equals(direction)) {
-                    direction = "E";
-                } else {
-                    direction = "S";
-                }
+                throw new RuntimeException("Unknown command");
             }
+            
         }
-        this.endPunkt = direction + ":0:" + y;
+        this.endPunkt = direction + ":" + x + ":" + y;
+    }
+
+
+    private void rotateLeft() {
+        if ("N".equals(direction)) {
+            direction = "W";
+        } else if ("E".equals(direction)) {
+            direction = "N";
+        } else if ("S".equals(direction)) {
+            direction = "E";
+        } else {
+            direction = "S";
+        }
+    }
+
+    private void rotateRight() {
+        if ("N".equals(direction)) {
+            direction = "E";
+        } else if ("E".equals(direction)) {
+            direction = "S";
+        } else if ("S".equals(direction)) {
+            direction = "W";
+        } else {
+            direction = "N";
+        }
+    }
+
+    private void moveForward() {
+        if ("N".equals(direction)) {
+            this.y++;
+        } else if ("E".equals(direction)) {
+            this.x++;
+        } else if ("W".equals(direction)) {
+            this.x--;
+        }else if ("S".equals(direction)){
+            this.y--;
+        }
+    }
+    
+    private void moveBackword() {
+        if ("N".equals(direction)) {
+            this.y--;
+        } else if ("E".equals(direction)) {
+            this.x--;
+        } else if ("W".equals(direction)) {
+            this.x++;
+        }else if ("S".equals(direction)){
+            this.y++;
+        }
     }
 }
+
